b5b8c3bf81d4eeca06a157b747ef40d55a958113
GIAKHOILEE <94039772+GIAKHOILEE@users.noreply.github.com>
2023-04-03 14:36:39 +0700

    feat file organization

---
+++ src/app/selectbooking/page.tsx
@@ -0,0 +1,8 @@
+import styles from './selectBooking.module.css'
+export default function SelectBooking() {
+    return (
+        <div className={styles.container}> 
+            Đây là trang select Booking
+        </div>
+    )
+}
