1eaae41404ec5dc7f25ae021689f32e035d6963e
Đỗ Vạn Lâm <85599572+lambo1207@users.noreply.github.com>
2023-04-06 22:49:16 +0700

    feat(admin): create all file
    update, create, layout,

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+import HeaderAdmin from "@/components/layout/HeaderAdmin/header_admin";
+import styles from "./admin.module.css";
+
+export default function Admin() {
+    return (
+        <>
+            <HeaderAdmin></HeaderAdmin>
+            <div>
+                <h1 className={styles.body}>oke</h1>
+            </div>
+        </>
+    )
+
+}
