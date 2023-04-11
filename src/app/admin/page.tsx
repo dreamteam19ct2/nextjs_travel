14b710463e8417fde529c626b79864975fae649f
GIAKHOILEE <94039772+GIAKHOILEE@users.noreply.github.com>
2023-04-11 08:24:19 +0700

    feat: get api tour

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,64 +3,117 @@
 import HeaderAdmin from "@/components/layout/HeaderAdmin/header_admin";
 import CardTour from "@/components/common/card-tour/card_tour";
 import styles from "./admin.module.css";
-
+import { useState, useEffect } from "react";
 export default function Admin() {
+    const [tourList, setTourList] = useState([]);
+    useEffect(() => {
+      async function fetchTourData() {
+        const response = await fetch(
+          "http://127.0.0.1:8000/api/login/get_tour"
+        );
+        const data = await response.json();
+        setTourList(data);
+      }
+      fetchTourData();
+    }, []);
+
     return (
-        <>
-            <HeaderAdmin></HeaderAdmin>
-            <div className={styles.body}>
-                <div className={styles.btn_add_tour}>
-                    <button className={styles.header__button}>Add Tour</button>
-                </div>
-                <div className={styles.select_tour}>
-                    <div className={styles.margin_right10}>Type Tour</div>
-                    <div className={`${styles.select_type} ${styles.margin_right30}`}>
-                        <select>
-                            <option value="option1">Popular</option>
-                            <option value="option2">Family</option>
-                            <option value="option3">Alone</option>
-                            <option value="option4">Company</option>
-                        </select>
-                    </div>
-                    <div className={styles.margin_right10}>Number of People</div>
-                    <div className={`${styles.select_type} ${styles.margin_right30}`}>
-                        <select>
-                            <option value="option1">1</option>
-                            <option value="option2">2</option>
-                            <option value="option3">3</option>
-                            <option value="option4">another</option>
-                        </select>
-                    </div>
-                    <div className={styles.margin_right10}>Days</div>
-                    <div className={styles.select_type}>
-                        <select>
-                            <option value="option1">2 days 1 night</option>
-                            <option value="option2">4 days 3 nights</option>
-                            <option value="option3">1 week</option>
-                            <option value="option4">2 week</option>
-                        </select>
-                    </div>
-                </div>
-                <div className={styles.popular_tour}>
-                    <div className={styles.popular_tour_title}>Popular Tour</div>
-                    <CardTour></CardTour>
-                    <CardTour></CardTour>
-                </div>
-
-                <div className={styles.family_tour}>
-                    <div className={styles.family_tour_title}>Family Tour</div>
-                    <CardTour></CardTour>
-                    <CardTour></CardTour>
-                    <CardTour></CardTour>
-                </div>
-
-                <div className={styles.new_tour}>
-                    <div className={styles.new_tour_title}>New Tour</div>
-                    <CardTour></CardTour>
-                    <CardTour></CardTour>
-                </div>
+      <>
+        <HeaderAdmin></HeaderAdmin>
+        <div className={styles.body}>
+          <div className={styles.btn_add_tour}>
+            <button className={styles.header__button}>Add Tour</button>
+          </div>
+          <div className={styles.select_tour}>
+            <div className={styles.margin_right10}>Type Tour</div>
+            <div className={`${styles.select_type} ${styles.margin_right30}`}>
+              <select className={styles.select}>
+                <option value="option1">Popular</option>
+                <option value="option2">Family</option>
+                <option value="option3">Alone</option>
+                <option value="option4">Company</option>
+              </select>
             </div>
-        </>
+            <div className={styles.margin_right10}>Number of People</div>
+            <div className={`${styles.select_type} ${styles.margin_right30}`}>
+              <select className={styles.select}>
+                <option value="option1">1</option>
+                <option value="option2">2</option>
+                <option value="option3">3</option>
+                <option value="option4">another</option>
+              </select>
+            </div>
+            <div className={styles.margin_right10}>Days</div>
+            <div className={styles.select_type}>
+              <select className={styles.select}>
+                <option value="option1">2 days 1 night</option>
+                <option value="option2">4 days 3 nights</option>
+                <option value="option3">1 week</option>
+                <option value="option4">2 week</option>
+              </select>
+            </div>
+          </div>
+          <div className={styles.popular_tour}>
+            <div className={styles.popular_tour_title}>Popular Tour</div>
+            {tourList
+              .filter((tour: any) => tour.type_tour === "popular")
+              .map((tour: any) => (
+                <CardTour
+                  tour_name={tour.tour_name}
+                  img={tour.img}
+                  description={tour.description}
+                  date_start={tour.date_start}
+                  date_end={tour.date_end}
+                  max_people={tour.max_people}
+                  price={tour.price}
+                  detail={tour.detail}
+                  type_tour={tour.type_tour}
+                  location={tour.max_people}
+                />
+              ))}
+          </div>
+
+          <div className={styles.family_tour}>
+            <div className={styles.family_tour_title}>Family Tour</div>
+            {tourList
+              .filter((tour: any) => tour.type_tour === "family")
+              .map((tour: any) => (
+                <CardTour
+                  tour_name={tour.tour_name}
+                  img={tour.img}
+                  description={tour.description}
+                  date_start={tour.date_start}
+                  date_end={tour.date_end}
+                  max_people={tour.max_people}
+                  price={tour.price}
+                  detail={tour.detail}
+                  type_tour={tour.type_tour}
+                  location={tour.max_people}
+                />
+              ))}
+          </div>
+
+          <div className={styles.new_tour}>
+            <div className={styles.new_tour_title}>New Tour</div>
+            {tourList
+              .filter((tour: any) => tour.type_tour === "new")
+              .map((tour: any) => (
+                <CardTour
+                  tour_name={tour.tour_name}
+                  img={tour.img}
+                  description={tour.description}
+                  date_start={tour.date_start}
+                  date_end={tour.date_end}
+                  max_people={tour.max_people}
+                  price={tour.price}
+                  detail={tour.detail}
+                  type_tour={tour.type_tour}
+                  location={tour.max_people}
+                />
+              ))}
+          </div>
+        </div>
+      </>
     );
 
 }
--- src/components/common/card-tour/card_tour.tsx
+++ src/components/common/card-tour/card_tour.tsx
@@ -1,40 +1,66 @@
 "use client";
 
 import styles from "./card_tour.module.css";
-import StarIcon from '@mui/icons-material/Star';
-import StarHalfIcon from '@mui/icons-material/StarHalf';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import StarIcon from "@mui/icons-material/Star";
+import StarHalfIcon from "@mui/icons-material/StarHalf";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-export default function CardTour() {
-    return (
-        <>
-            <div className={styles.container}>
-                <div className={styles.body}>
-                    <img src="plane.png" alt="imgTour" />
-                    <div className={styles.content}>
-                        <div className={styles.content_top}>
-                            <div className={styles.content_name_tour}>Bana Hill</div>
-                            <div className={styles.count_star}>
-                                <StarIcon className={styles.star_icon} />
-                                <StarIcon className={styles.star_icon} />
-                                <StarIcon className={styles.star_icon} />
-                                <StarIcon className={styles.star_icon} />
-                                <StarHalfIcon className={styles.star_icon} />
-                            </div>
-                        </div>
-                        <div className={styles.content_mid}>
-                            a cool trip because the various scenery around will be very eye-catching
-                        </div>
-                        <div className={styles.content_bot}>
-                            <div className={styles.schedule}>Schedule :  </div>
-                            <div className={styles.date}>Feb 5, 2023 - Feb 14, 2023 </div>
-                            <div className={styles.number_people}>Number of people :  </div>
-                            <div className={styles.people}>20 people </div>
-                        </div>
-                    </div>
-                    <ArrowForwardIosIcon className={styles.arrow_icon}/>
-                </div>
+interface Props {
+  tour_name: string;
+  img: string;
+  description: string;
+  date_start: string;
+  date_end: string;
+  max_people: string;
+  price: number;
+  detail: string;
+  type_tour: string;
+  location: string;
+  //   slug: string;
+}
+
+export default function CardTour({
+  tour_name,
+  img,
+  description,
+  date_start,
+  date_end,
+  max_people,
+  price,
+  detail,
+  type_tour,
+  location,
+}: Props) {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.body}>
+          {/* <img src="plane.png" alt="imgTour" /> */}
+          <img src={img} alt="imgTour" />
+          <div className={styles.content}>
+            <div className={styles.content_top}>
+              <div className={styles.content_name_tour}>{tour_name}</div>
+              <div className={styles.count_star}>
+                <StarIcon className={styles.star_icon} />
+                <StarIcon className={styles.star_icon} />
+                <StarIcon className={styles.star_icon} />
+                <StarIcon className={styles.star_icon} />
+                <StarHalfIcon className={styles.star_icon} />
+              </div>
             </div>
-        </>
-    );
+            <div className={styles.content_mid}>{description}</div>
+            <div className={styles.content_bot}>
+              <div className={styles.schedule}>Schedule : </div>
+              <div className={styles.date}>
+                {date_start} - {date_end}
+              </div>
+              <div className={styles.number_people}>Number of people : </div>
+              <div className={styles.people}>{max_people} people </div>
+            </div>
+          </div>
+          <ArrowForwardIosIcon className={styles.arrow_icon} />
+        </div>
+      </div>
+    </>
+  );
 }
