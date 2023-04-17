2ab1dfc74375c76d202fea23afcbf16366bd523e
NTTien1211 <90597279+NTTien1211@users.noreply.github.com>
2023-04-17 20:56:30 +0700

    checktour and userprofile

---
+++ src/app/check_tour/page.tsx
@@ -0,0 +1,75 @@
+"use client"
+import styles from  "./check_tour.module.css";
+import React, { useState } from 'react';
+import StarHalfSharpIcon from '@mui/icons-material/StarHalfSharp';
+import StarPurple500SharpIcon from '@mui/icons-material/StarPurple500Sharp';
+import Headere from "@/components/layout/Header/header";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+export default function tour_detail(){
+    const [isBorderRed, setIsBorderRed] = useState(false);
+
+    const handleClick = () => {
+      setIsBorderRed(!isBorderRed);
+    }
+  
+    const borderStyle = {
+      border: `10px solid ${isBorderRed ? 'green' : 'red'}`,
+    };
+
+
+
+        return(
+            <>
+                     <Headere></Headere>
+                     <div className={styles.check_tour__contain}>
+                              <h1 className={styles.container__header__checkTour__Name}>Check tour</h1>
+
+                                <div className={styles.animation_container}>
+                                        <div className={styles.circle}></div>
+                                        <div className={styles.circle}></div>
+                                        <div className={styles.circle}></div>
+                                        <div className={styles.circle}></div>
+                                        <div className={styles.circle}></div>
+                                        <div className={styles.circle}></div>
+                                    </div>
+                                <div style={{display: 'flex'}} className={styles.container__header__checkTour__Name_inf_main}>
+                                         <div className={styles.container__header__checkTour__Name_inf}>
+                                                     <p className={styles.container__header__checkTour__Name_inf__p }  style={borderStyle}>Status</p>
+                                        </div>
+                                        <div  className={styles.container__header__checkTour__Name_inf__foot}>
+                                            <div className={styles.container__header__checkTour__Name__inf__left}>
+                                                                <p className={styles.container__header__checkTour__Name__inf__left__p}>Name tour</p>
+                                                        </div>
+                                                        <div     className={styles.container__header__checkTour__Name__inf__left__p_lr} >
+                                                                        <div  className={styles.container__header__checkTour__Name__inf__left__p_lr__}>
+                                                                                                <p className={styles.container__header__checkTour__Name__inf__left__p__p}>Start day</p>
+                                                                                                <label className={styles.container__header__checkTour__Name__inf__left__p__label} htmlFor="">14/5/2023</label>
+                                                                        </div>
+                                                                        <div  className={styles.container__header__checkTour__Name__inf__left__p_lr_}>
+                                                                                                <p className={styles.container__header__checkTour__Name__inf__left__p__p}>End day</p>
+                                                                                                <label className={styles.container__header__checkTour__Name__inf__left__p__label} htmlFor="">14/5/2023</label>
+                                                                        </div>
+                                                        </div>
+                                                        <div  className={styles.container__header__checkTour__Name__inf__about}>
+                                                                    <p className={styles.container__header__checkTour__Name__inf__about__p}>Đặt ngay vé tham quan du lịch và các tours để chiêm ngưỡng các địa danh hấp dẫn, hot nhất trong và ngoài nước cùng nhiều ưu đãi hấp dẫn. </p>
+                                                                    <div className={styles.container__header__checkTour__Name__inf__about__label}>
+                                                                            <label> $70.00 </label>
+                                                                    </div>
+                                                                   
+                                                        </div>
+                                                        <div className={styles.container__header__checkTour__Name__inf__about__main} >
+                                                                <button onClick={handleClick} className={styles.container__header__checkTour__Name__inf__about__btn}>Browse</button>
+                                                        </div>
+                                        </div>
+                                </div>
+                             
+                              
+                     </div>
+                    
+
+            </>
+            
+        )
+        
+ }
+
--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -8,46 +8,46 @@ export default function payment(){
             <>
                 <Headere></Headere>
                 <div  className={styles.container_payment}>
-                    <div className={styles.container__header__nameTourDetail}>
+                    <div className={styles.container__header__namePayment}>
                                             <ArrowBackIosIcon />
-                                            <h1 className={styles.container__header__nameTourDetail__Name}>BaNa Hill</h1>
+                                            <h1 className={styles.container__header__namePayment__Name}>BaNa Hill</h1>
                      </div>
                     <form action="">
-                        <div  className={styles.container__header__nameTourDetail__content}>
-                                    <div className={styles.container__header__nameTourDetail__content_item}>
-                                            <div className={styles.container__header__nameTourDetail__content__name__firstname}>
-                                                    <p className={styles.container__header__nameTourDetail__content__p}>First Name :  </p>
-                                                    <input readOnly  className={styles.container__header__nameTourDetail__content__name__view}></input>
+                        <div  className={styles.container__header__namePayment__content}>
+                                    <div className={styles.container__header__namePayment__content_item}>
+                                            <div className={styles.container__header__namePayment__content__name__firstname}>
+                                                    <p className={styles.container__header__namePayment__content__p}>First Name :  </p>
+                                                    <input readOnly  className={styles.container__header__namePayment__content__name__view}></input>
                                             </div>
-                                            <div className={styles.container__header__nameTourDetail__content__name__lastname}>
-                                                    <p className={styles.container__header__nameTourDetail__content__p}>Last Name :  </p>
-                                                    <input readOnly  className={styles.container__header__nameTourDetail__content__name__view}></input>
+                                            <div className={styles.container__header__namePayment__content__name__lastname}>
+                                                    <p className={styles.container__header__namePayment__content__p}>Last Name :  </p>
+                                                    <input readOnly  className={styles.container__header__namePayment__content__name__view}></input>
                                             </div>
                                     </div>
-                                    <div className={styles.container__header__nameTourDetail__content_item}>
-                                                    <p className={styles.container__header__nameTourDetail__content__p}>Phone Number  :  </p>
-                                                    <input readOnly  className={styles.container__header__nameTourDetail__content__view}></input>
+                                    <div className={styles.container__header__namePayment__content_item}>
+                                                    <p className={styles.container__header__namePayment__content__p}>Phone Number  :  </p>
+                                                    <input readOnly  className={styles.container__header__namePayment__content__view}></input>
                                     </div>
-                                    <div className={styles.container__header__nameTourDetail__content_item}>
-                                                    <p className={styles.container__header__nameTourDetail__content__p}>Email Address :   </p>
-                                                    <input readOnly  className={styles.container__header__nameTourDetail__content__view}></input>
+                                    <div className={styles.container__header__namePayment__content_item}>
+                                                    <p className={styles.container__header__namePayment__content__p}>Email Address :   </p>
+                                                    <input readOnly  className={styles.container__header__namePayment__content__view}></input>
                                     </div>
-                                    <div className={styles.container__header__nameTourDetail__content_item}>
-                                                    <p className={styles.container__header__nameTourDetail__content__p}>Number of People :   </p>
-                                                    <input readOnly  className={styles.container__header__nameTourDetail__content__view}></input>
+                                    <div className={styles.container__header__namePayment__content_item}>
+                                                    <p className={styles.container__header__namePayment__content__p}>Number of People :   </p>
+                                                    <input readOnly  className={styles.container__header__namePayment__content__view}></input>
                                     </div>
-                                    <div className={styles.container__header__nameTourDetail__content_item}>
-                                                    <p className={styles.container__header__nameTourDetail__content__p}>Address:  </p>
-                                                    <input readOnly  className={styles.container__header__nameTourDetail__content__view}></input>
+                                    <div className={styles.container__header__namePayment__content_item}>
+                                                    <p className={styles.container__header__namePayment__content__p}>Address:  </p>
+                                                    <input readOnly  className={styles.container__header__namePayment__content__view}></input>
                                     </div>
                                     
                         </div>
-                        <div className={styles.container__header__nameTourDetail__detail_price}> 
-                                         <div className={styles.container__header__nameTourDetail__detail_price__nomal} >
-                                                <p className={styles.container__header__nameTourDetail__detail_price__nomal_p}>Total Price</p>
+                        <div className={styles.container__header__namePayment__detail_price}> 
+                                         <div className={styles.container__header__namePayment__detail_price__nomal} >
+                                                <p className={styles.container__header__namePayment__detail_price__nomal_p}>Total Price</p>
                                                 <p>100$</p>    
                                          </div>
-                                         <button className={styles.container__header__nameTourDetail__detail_price_button}>Payment</button>
+                                         <button className={styles.container__header__namePayment__detail_price_button}>Payment</button>
                         </div>
                     </form>
                 </div>
