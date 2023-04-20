99d033f9ad98feed8d9a2359920cedbfd974b14a
GIAKHOILEE <94039772+GIAKHOILEE@users.noreply.github.com>
2023-04-20 09:53:49 +0700

    feat: tourDetail

--- src/app/tour_detail/page.tsx
+++ src/app/tour_detail/page.tsx
@@ -1,83 +1,118 @@
-"use client"
-import styles from  "./tour_detail.module.css";
-import StarHalfSharpIcon from '@mui/icons-material/StarHalfSharp';
-import StarPurple500SharpIcon from '@mui/icons-material/StarPurple500Sharp';
+"use client";
+import styles from "./tour_detail.module.css";
+import StarHalfSharpIcon from "@mui/icons-material/StarHalfSharp";
+import StarPurple500SharpIcon from "@mui/icons-material/StarPurple500Sharp";
 import Headere from "@/components/layout/Header/header";
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-export default function tour_detail(){
-        return(
-                <>
-                    <Headere></Headere>
-                    <div className={styles.container}>
-                        <div  className={styles.container__header}>
-                                <div className={styles.container__header__nameTourDetail}>
-                                        <ArrowBackIosIcon />
-                                        <h1 className={styles.container__header__nameTourDetail__Name}>BaNa Hill</h1>
-                                </div>
-                                <div className={styles.container__header__nameTourDetail__img}>
-                                        <img src="/Rectangle1055.png" alt="" />
-                                </div>
-                                <div className={styles.container__header__nameTourDetail__content}> 
-                                        <p style={{ fontSize: '16px' }}>Theme parks may not be to every traveller’s taste. But Sun World Ba Na Hills − the European-themed recreational complex in the Trường Sơn Mountains, about 30km from Danang – certainly draws the crowds. That’s because it offers every visitor, of all ages , an entertaining day out in cool climes.</p>
-                                </div>
-                                <div className={styles.container__header__nameTourDetail__Evaluate}>
-                                                <p>Evaluate</p>
-                                                <div className={styles.container__header__nameTourDetail__Evaluate__icon}>
-                                                                <StarPurple500SharpIcon className={styles.container__header__nameTourDetail__Evaluate__icon__icon}/>
-                                                                <StarPurple500SharpIcon className={styles.container__header__nameTourDetail__Evaluate__icon__icon}/>
-                                                                <StarPurple500SharpIcon className={styles.container__header__nameTourDetail__Evaluate__icon__icon}/>
-                                                                <StarPurple500SharpIcon className={styles.container__header__nameTourDetail__Evaluate__icon__icon}/>
-                                                                <StarHalfSharpIcon className={styles.container__header__nameTourDetail__Evaluate__icon__icon}/>
-                                                </div>
-                                </div>
-                                <div className={styles.container__header__nameTourDetail__Schedule }>
-                                                <p>Schedule: </p>
-                                                <div className={styles.container__header__nameTourDetail__Schedule__p}>
-                                                                <p>Feb 5, 2023 - Feb 10, 2023 </p>  
-                                                </div>
-                                </div>
-                                <div className={styles.container__header__nameTourDetail__people }>
-                                                <p>Number of people:  </p>
-                                                <div className={styles.container__header__nameTourDetail__people__quantity}>
-                                                                <p>20 people </p>  
-                                                </div>
-                                </div>
-                                <div className={styles.container__header__nameTourDetail__detail}>
-                                         <div className={styles.container__header__nameTourDetail__detail__day}>
-                                                 <p className={styles.container__header__nameTourDetail__detail__day__text}>Day 1</p>
-                                                 <p  className={styles.container__header__nameTourDetail__detail__day__text__contain}>Take a bus from bmt to DaNang</p>
-                                         </div>
-                                         <div className={styles.container__header__nameTourDetail__detail__day}>
-                                                 <p className={styles.container__header__nameTourDetail__detail__day__text}>Day 2</p>
-                                                 <p  className={styles.container__header__nameTourDetail__detail__day__text__contain}>Transfer to hotel in the city center. Walk around the old town and enjoy street food</p>
-                                         </div>
-                                         <div className={styles.container__header__nameTourDetail__detail__day}>
-                                                 <p className={styles.container__header__nameTourDetail__detail__day__text}>Day 3</p>
-                                                 <p  className={styles.container__header__nameTourDetail__detail__day__text__contain}>Visit famous places like Wat Phra Kaew, Grand Palace, Wat Pho and sunbathe at Pattaya beach in the afternoon.</p>
-                                         </div>
-                                         <div className={styles.container__header__nameTourDetail__detail__day}>
-                                                 <p className={styles.container__header__nameTourDetail__detail__day__text}>Day 4</p>
-                                                 <p  className={styles.container__header__nameTourDetail__detail__day__text__contain}>Fly to Ho Chi Minh City, Vietnam to visit famous sites like Tao Dan Park, Ben Thanh Market, Reunification Palace, and Independence Palace</p>
-                                         </div>
-                                         <div className={styles.container__header__nameTourDetail__detail__day}>
-                                                 <p className={styles.container__header__nameTourDetail__detail__day__text}>Day 5</p>
-                                                 <p  className={styles.container__header__nameTourDetail__detail__day__text__contain}>Visit Hoi An Ancient City and Ba Na Hills in Da Nang, Vietnam. Fly back to Bangkok to end the tour</p>
-                                         </div>
-                                         
-                                </div>
-                                <div className={styles.container__header__nameTourDetail__detail_price}> 
-                                         <p style={{ fontWeight: 700, fontSize: '40px', color: '#F97300'}}>Price</p>
-                                         <div className={styles.container__header__nameTourDetail__detail_price_money}>
-                                                <p>100$</p>
-                                                <p>/person</p>
-                                         </div>
-                                         <button className={styles.container__header__nameTourDetail__detail_price_button}>Booking Now</button>
-                                </div>
-                        </div>
-                        
-                    </div>
-                </>
-        )
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import Grid from "@mui/material/Grid";
+import { useState, useEffect } from "react";
 
+export default function tour_detail() {
+  const [tourList, setTourList] = useState([]);
 
+  useEffect(() => {
+    async function fetchTourData() {
+      const response = await fetch("http://127.0.0.1:8000/api/get_tour");
+      const data = await response.json();
+      setTourList(data);
+    }
+    fetchTourData();
+  }, []);
+
+    const tourId = localStorage.getItem("selectedTourId");
+
+  return (
+    <>
+      <Headere></Headere>
+      {tourList
+        .filter((tour: any) => tour.id == tourId)
+        .map((tour: any) => (
+          <div className={styles.container}>
+            <div className={styles.TourDetail}>
+              <ArrowBackIosIcon />
+              <h1 className={styles.TourDetail__Name}>{tour.tour_name}</h1>
+            </div>
+            <img className={styles.TourDetail__img} src={tour.img} alt="img" />
+            <div className={styles.TourDetail__content}>{tour.description}</div>
+            <div className={styles.TourDetail}>
+              <span className={styles.title__left}>Evaluate</span>
+              <StarPurple500SharpIcon
+                className={styles.TourDetail__Evaluate__icon__icon}
+              />
+              <StarPurple500SharpIcon
+                className={styles.TourDetail__Evaluate__icon__icon}
+              />
+              <StarPurple500SharpIcon
+                className={styles.TourDetail__Evaluate__icon__icon}
+              />
+              <StarPurple500SharpIcon
+                className={styles.TourDetail__Evaluate__icon__icon}
+              />
+              <StarHalfSharpIcon
+                className={styles.TourDetail__Evaluate__icon__icon}
+              />
+            </div>
+            <div className={styles.TourDetail}>
+              <span className={styles.title__left}>Schedule: </span>
+              <span className={styles.content__right}>
+                {tour.date_start} - {tour.date_end}
+              </span>
+            </div>
+            <div className={styles.TourDetail}>
+              <span className={styles.title__left}>Number of people: </span>
+              <span className={styles.content__right}>
+                {tour.max_people} people
+              </span>
+            </div>
+
+            <Grid container spacing={2} className={styles.TourDetail}>
+              <Grid item xs={1.5}>
+                <div className={styles.grid__schedule__day}>Day 1</div>
+              </Grid>
+              <Grid item xs={10.5} className={styles.grid__schedule__detail}>
+                Take a bus from bmt to DaNang
+              </Grid>
+              <Grid item xs={1.5}>
+                <div className={styles.grid__schedule__day}>Day 2</div>
+              </Grid>
+              <Grid item xs={10.5} className={styles.grid__schedule__detail}>
+                Transfer to hotel in the city center. Walk around the old town
+                and enjoy street food
+              </Grid>
+              <Grid item xs={1.5}>
+                <div className={styles.grid__schedule__day}>Day 3</div>
+              </Grid>
+              <Grid item xs={10.5} className={styles.grid__schedule__detail}>
+                Visit famous places like Wat Phra Kaew, Grand Palace, Wat Pho
+                and sunbathe at Pattaya beach in the afternoon.
+              </Grid>
+              <Grid item xs={1.5}>
+                <div className={styles.grid__schedule__day}>Day 4</div>
+              </Grid>
+              <Grid item xs={10.5} className={styles.grid__schedule__detail}>
+                Fly to Ho Chi Minh City, Vietnam to visit famous sites like Tao
+                Dan Park, Ben Thanh Market, Reunification Palace, and
+                Independence Palace
+              </Grid>
+              <Grid item xs={1.5}>
+                <div className={styles.grid__schedule__day}>Day 5</div>
+              </Grid>
+              <Grid item xs={10.5} className={styles.grid__schedule__detail}>
+                Visit Hoi An Ancient City and Ba Na Hills in Da Nang, Vietnam.
+                Fly back to Bangkok to end the tour
+              </Grid>
+            </Grid>
+            <div className={styles.TourDetail__booking}>
+              <div>
+                <span className={styles.tourDetail__price}>Price </span>
+                <span className={styles.tourDetail__price__person}>
+                  {tour.price}$/person
+                </span>
+              </div>
+              <button className={styles.TourDetail__button}>Booking Now</button>
+            </div>
+          </div>
+        ))}
+    </>
+  );
 }
